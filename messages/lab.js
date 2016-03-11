2b05f19f823187968ba74ae9f1f157e65b89685a
nshash01 <nshash01@cs.tufts.edu>
2016-03-10 20:05:24 -0500

    Added js message requesting and CSS styling

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,25 @@
 // Your JavaScript goes here...
+request = new XMLHttpRequest();
+
+request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == 200) {
+                var raw = request.responseText;
+                var messages = JSON.parse(raw);
+                console.log(messages);
+
+                var elem = document.getElementById("messages");
+
+                var length = messages.length;
+                for (i = 0; i < length; i++) {
+                        var content = messages[i].content;
+                        var username = messages[i].username;
+                        elem.innerHTML += "<p class='msg'>" + content +
+                                          " <span class='usr'>" + username +
+                                          "</span></p>";
+                }
+    }
+}
+
+request.open("GET", "data.json", true);
+
+request.send(null);
